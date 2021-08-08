5b895480517aeaeedebc5883b4a7e9ace571a9c3
Maksim Shilov <marvil08@gmail.com>
2021-08-08 18:11:59 +0300

    vol 5

---
+++ src/05-led-roulette/_openocd.gdb
@@ -0,0 +1,23 @@
+target remote :3333
+
+load
+
+set print asm-demangle on
+
+set print pretty on
+
+set style sources off
+
+#monitor tpiu config internal itm.txt uart off 8000000
+
+#monitor itm port 0 on
+
+break main
+
+#break DefaultHandler
+
+#break HardFault
+
+continue
+
+step
--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,21 +2,40 @@
 #![no_main]
 #![no_std]
 
-use volatile::Volatile;
 use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let mut half_period = 500_u16;
-    let v_half_period = Volatile::new(&mut half_period);
+    let period = 500_u16;
+
+    let led_count: usize = 8;
+    let mut current: usize = 0;
+    let mut next: usize = 1;
+    let mut prev: usize = 7;
 
     loop {
-        leds[0].on().ok();
-        delay.delay_ms(v_half_period.read());
-
-        leds[0].off().ok();
-        delay.delay_ms(v_half_period.read());
+        
+        //step 1
+        leds[current].on().ok();
+        leds[prev].on().ok();
+        delay.delay_ms(period);
+        
+        //step2
+        leds[prev].off().ok();
+        delay.delay_ms(period);
+        
+        current = update_pin(current, led_count);
+        prev = update_pin(prev, led_count);
     }
+}
+
+fn update_pin (mut led: usize, count: usize) -> usize{
+    if (led+1) < count {
+        led +=1;
+    } else {
+        led = 0;
+    }
+    led
 }
